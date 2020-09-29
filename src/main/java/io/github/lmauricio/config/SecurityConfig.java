cd29d30ab090f3085e3bec6ab9d17e64aae52b05
Leonardo Mauricio <leomauricio7@gmail.com>
2020-09-29 14:26:58 -0300

    add in roles permissions

--- src/main/java/io/github/lmauricio/config/SecurityConfig.java
+++ src/main/java/io/github/lmauricio/config/SecurityConfig.java
@@ -24,16 +24,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Ex: autenticacao em memoria
         auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("USER");
+                .passwordEncoder(passwordEncoder()) // metodo de criptografia
+                .withUser("admin") // user
+                .password(passwordEncoder().encode("admin")) // senha
+                .roles("USER"); // perfil
+        //.authorities("ADMIN"); // autorizacao
     }
 
     @Override
     // metodo de configurar a permissao dos usuarios
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        /*
+         *   PERMISSÕES DE USUÁRIO
+         *   - hasAuthority - autorizacao
+         *   - hasRole -  perfil
+         *   - permitAll - aberta
+         *   - authenticated - basta ta logado
+         * */
+        http
+                .csrf().disable() // desabilita uma conf de permisao de seguranca ente uma api ewb e o back end
+                .authorizeRequests() // autorizacoes dos requests
+                .antMatchers("/api/clientes/**") // rotas
+                .hasRole("ADMIN") //permissao
+                .and()
+                .formLogin(); // criar o formulario de login default
+
     }
 
 }

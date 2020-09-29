4b526265511331146023a0a1ecf1ecf319f86d7c
Leonardo Mauricio <leomauricio7@gmail.com>
2020-09-29 13:24:39 -0300

    password encode

--- src/main/java/io/github/lmauricio/config/SecurityConfig.java
+++ src/main/java/io/github/lmauricio/config/SecurityConfig.java
@@ -1,17 +1,26 @@
 package io.github.lmauricio.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // classe de configuracao do spring-security
 @EnableWebSecurity // anotation de ewb security
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // metodos para configurar o spring security
 
+    // bean para criptograr e descriptografar a senha de um usuário
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
-    //metodo de configuracao da parte de autenticacao
+    //metodo de configuracao da parte de autenticacao do usuário
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);
     }

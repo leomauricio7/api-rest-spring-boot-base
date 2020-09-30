69cf965515b4db08b993317765b830a9284a6cdf
Leonardo Mauricio <leomauricio7@gmail.com>
2020-09-30 02:41:54 -0300

    configurando o jwt no swagger

--- src/main/java/io/github/lmauricio/config/SwaggerConfig.java
+++ src/main/java/io/github/lmauricio/config/SwaggerConfig.java
@@ -5,38 +5,68 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket docket(){
-    return new Docket(DocumentationType.SWAGGER_2)
-            .useDefaultResponseMessages(false)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("io.github.lmauricio.rest.controller"))
-            .paths(PathSelectors.any())
-            .build()
-            .apiInfo(apiInfo());
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.github.lmauricio.rest.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .apiInfo(apiInfo());
     }
 
-    private ApiInfo apiInfo(){
-        return  new ApiInfoBuilder()
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
                 .title("Vendas API")
                 .description("API do projeto de vendas com spring boot")
                 .version("1.0.0")
                 .contact(contact())
                 .build();
     }
-    private Contact contact(){
-        return  new Contact("Leonardo Mauricio",
+
+    private Contact contact() {
+        return new Contact("Leonardo Mauricio",
                 "https://github.com/leomauricio7",
                 "leomauricio7@gmail.com");
     }
+
+    public ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext
+                .builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference references = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(references);
+        return auths;
+    }
+
 }

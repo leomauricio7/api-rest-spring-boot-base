24306f05d97b0042d79b067611ab3bcce40b6dd8
Leonardo Mauricio <leomauricio7@gmail.com>
2020-09-30 00:43:44 -0300

    finalizanod a configuracao do JWt no spring security

--- src/main/java/io/github/lmauricio/config/SecurityConfig.java
+++ src/main/java/io/github/lmauricio/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.github.lmauricio.config;
 
+import io.github.lmauricio.security.jwt.JwtAuthFilter;
+import io.github.lmauricio.security.jwt.JwtService;
 import io.github.lmauricio.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +11,14 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 // classe de configuracao do spring-security
-// anotation de ewb security
+// anotation de web security
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${security.enable.csrf}")
@@ -21,6 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UsuarioServiceImpl usuarioService;
+
+    @Autowired
+    private JwtService jwtService;
 
     // metodos para configurar o spring security
 
@@ -30,8 +38,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
+    // bean para filtar o JWT do request
+    @Bean
+    public OncePerRequestFilter jwtFilter(){
+        return  new JwtAuthFilter(jwtService, usuarioService);
+    }
+
     //metodo de configuracao da parte de autenticacao do usuário
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Ex: autenticacao em banco auth basic
         auth.
@@ -39,8 +53,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder());
     }
 
-    @Override
     // metodo de configurar a permissao dos usuarios
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
         /*
@@ -49,6 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
          *   - hasRole -  perfil
          *   - permitAll - aberta
          *   - authenticated - basta ta logado
+         *
          * */
 
         http.csrf().disable();
@@ -64,8 +79,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/api/usuarios/**")
                     .permitAll()
                 //.anyRequest().authenticated()
-                .and()
-                    .httpBasic();
+                .and() // volta pra raiz httpSecurity
+                    .sessionManagement() // informa que vai usar sessions
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // sesions do tipo STATELESS
+                .and() // volta pra raiz do httpSecurity
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class); // colocar dentro da sessao o usuario do filtro do jwt
 
 //        if (csrfEnabled) {
 //            System.out.println("csrfEnabled: " + csrfEnabled);

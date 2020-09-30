9831f92992199819ce16f66cbd146968c8339eed
Leonardo Mauricio <leomauricio7@gmail.com>
2020-09-30 00:11:59 -0300

    decodificanod token

--- src/main/java/io/github/lmauricio/JwtService.java
+++ src/main/java/io/github/lmauricio/JwtService.java
@@ -1,9 +1,13 @@
 package io.github.lmauricio;
 
 import io.github.lmauricio.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -31,5 +35,46 @@ public class JwtService {
                 .setExpiration(data)
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura)
                 .compact();
+    }
+
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura) // passa a chave de assinatura
+                .parseClaimsJws(token) // pega o token enviado
+                .getBody(); // retorna os claims do token jwt
+    }
+
+    public boolean tokenValido(String token) {
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            // converter objeto do tipo DATE em LOCALDATETIME
+            LocalDateTime data = dataExpiracao.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            //verifica se a data atual não é depois da data da expiracao do token
+            return !LocalDateTime.now().isAfter(data);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException {
+        return obterClaims(token).getSubject();
+    }
+
+    // metodo principal para testar a implementacao
+    public static void main(String[] args) {
+        ConfigurableApplicationContext contexto = SpringApplication.run(VendasApplication.class);
+        JwtService service = contexto.getBean(JwtService.class);
+        Usuario usuario = new Usuario();
+        usuario.setLogin("fulano");
+        usuario.setSenha("123");
+        String token = service.gerarToken(usuario);
+        System.out.println("token: "+ token);
+
+        boolean isTokenValido = service.tokenValido(token);
+        System.out.println("Token valido: "+isTokenValido);
+
+        System.out.println("login token: "+ service.obterLoginUsuario(token));
     }
 }
